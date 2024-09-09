3c5247c0ecbfc4272fdfbedb49e73906154cdd20
refalinoshahzada <ref4gha@gmail.com>
2024-09-09 18:07:47 +0700

    membut template dan menggabungkan dengan view, melakukan django test dan membuat model dasar

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class MoodEntry(models.Model):
+class AlbumEntry(models.Model):
     name = models.CharField(max_length=255)
     price = models.DateField(auto_now_add=True)
     description = models.TextField()
-    data_of_distribution = models.TextField()
+    date_of_distribution = models.TextField()
     stock_available = models.IntegerField()
     genre = models.TextField()
--- main/views.py
+++ main/views.py
@@ -5,7 +5,10 @@ def show_main(request):
     context = {
         'name': 'Dark Side of the Moon',
         'price' : '650000',
-
+        'description' : 'The Dark Side of the Moon is the eighth studio album by the English rock band Pink Floyd',
+        'date_of_distribution' : '1 March 1973',
+        'stock_available' : 10,
+        'genre' : 'rock'
     }
 
     return render(request, "main.html", context)
