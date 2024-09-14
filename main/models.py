b4129d3a116440a6f7d8613f52dfb051bf35b43e
refalinoshahzada <ref4gha@gmail.com>
2024-09-14 22:27:57 +0700

    made base.html as a template for this project, set up uuid

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class AlbumEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.DateField(auto_now_add=True)
     description = models.TextField()
