c0fbc623ae181e9fb6927fc054e9ada41d3901f4
refalinoshahzada <ref4gha@gmail.com>
2024-09-19 08:52:22 +0700

    added register, login and logout system and required login before entering main site

--- main/views.py
+++ main/views.py
@@ -3,8 +3,13 @@ from main.forms import AlbumEntryForm
 from main.models import AlbumEntry
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     album_entries = AlbumEntry.objects.all()
 
@@ -20,6 +25,7 @@ def show_main(request):
 
     return render(request, "main.html", context)
 
+# Create album entry
 def create_album_entry(request):
     form = AlbumEntryForm(request.POST or None)
 
@@ -30,6 +36,7 @@ def create_album_entry(request):
     context = {'form': form}
     return render(request, "create_album_entry.html", context)
 
+# Show data through XML and JSON
 def show_xml(request):
     data = AlbumEntry.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
@@ -45,3 +52,36 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = AlbumEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+# Register User
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+# Login User
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+# Logout User
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
