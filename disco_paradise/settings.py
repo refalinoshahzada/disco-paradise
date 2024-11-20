d80827edca189359b19bc20eb367d976b39a0401
refalinoshahzada <ref4gha@gmail.com>
2024-11-20 11:07:57 +0700

    tugas 9

--- disco_paradise/settings.py
+++ disco_paradise/settings.py
@@ -27,8 +27,15 @@ SECRET_KEY = 'django-insecure-5q4@l41afr&=q#iv#ve5mxgx&p_14^m8q0^oag8x2twf5^new%
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "refalino-shahzada-discoparadisee.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "refalino-shahzada-discoparadisee.pbp.cs.ui.ac.id", "10.0.2.2"]
 
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 # Application definition
 
@@ -40,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'disco_paradise.urls'
